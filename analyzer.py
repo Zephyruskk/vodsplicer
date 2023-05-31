678bcf21ca0e12ea98b4f1e9c6cbe264e9a2516d
zephyruskk <zscott2212@gmail.com>
2023-05-30 22:45:57 -0400

    Refine identification technique.

--- analyzer.py
+++ analyzer.py
@@ -6,12 +6,17 @@ import threading
 import multiprocessing as mp
 
 
-p2_temp_threshold = 0.75
+temp_threshold = 0.75
 # x1, y1 = 960,20
 # x2, y2 = 1080, 105
 
+go_coords = 495,195,1305,505 
 p1_coords = 20,40,80,90 # 10,20,100,100 # x1,y1,x2,y2
 p2_coords = 980,40,1046,90 # 970,20,1060,100
+p3_coords = 19,40,85,90
+p4_coords = 980,40,1046,90
+
+player_icon_coords = 0,0,1700,90
 
 batch_size = 16 # how many frames get checked at once (thread count!)
 batch_item_length = 30 # frames long
@@ -23,7 +28,7 @@ game_start_frames = False
 
 
 
-def process_frame(frame, frame_number, template, region_of_interest):
+def process_frame(frame, frame_number, templates, region_of_interest):
     global game_start_times
 
     x1,y1,x2,y2 = region_of_interest
@@ -32,24 +37,43 @@ def process_frame(frame, frame_number, template, region_of_interest):
 
     roi = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
 
-    res = cv.matchTemplate(roi, template, cv.TM_CCOEFF_NORMED)
-    # Find the location of the maximum correlation coefficient in the ROI
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    for t in templates:
+        res = cv.matchTemplate(roi, t, cv.TM_CCOEFF_NORMED)
 
-    if max_val <= p2_temp_threshold:
-        return None
+        # Find the location of the maximum correlation coefficient in the ROI
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-    game_start_times.append((frame_number, frame))
+        if max_val >= temp_threshold:
+            game_start_times.append((frame_number, max_val, frame))
+            return True # match found 
+        
+    return False # no match found
+
+        
 
 # load in keyframes, batch_size at a time
 def process_video():
 
     vid_path = "./media/sample_0001.mkv"
+    go_path = "./media/go.png"
+    p1_path = "./media/p1.png"
+    p2_path = "./media/p2.png"
+    p3_path = "./media/p3.png"
+    p4_path = "./media/p4.png"
+
     capture = cv.VideoCapture(vid_path)
-    # capture = cv.VideoCapture("../obs_rec/aop-10_5_23/sample_Trim.mp4")
-    p2_template = "./media/p2.png"
-    p2_template = cv.imread(p2_template)
+
+    go_template = cv.imread(go_path)
+    go_template = cv.cvtColor(go_template, cv.COLOR_BGR2GRAY)
+    p1_template = cv.imread(p1_path)
+    p1_template = cv.cvtColor(p1_template, cv.COLOR_BGR2GRAY)  # convert to grayscale
+    p2_template = cv.imread(p2_path)
     p2_template = cv.cvtColor(p2_template, cv.COLOR_BGR2GRAY)  # convert to grayscale
+    p3_template = cv.imread(p3_path)
+    p3_template = cv.cvtColor(p3_template, cv.COLOR_BGR2GRAY)  # convert to grayscale
+    p4_template = cv.imread(p4_path)
+    p4_template = cv.cvtColor(p4_template, cv.COLOR_BGR2GRAY)  # convert to grayscale
+
     p2_t_h, p2_t_w = p2_template.shape[::1]
 
     if not capture.isOpened():
@@ -85,7 +109,7 @@ def process_video():
             for f in frames:
                 if f is not None:
                     # print(f"Thread for frame {f[0]} starting...")
-                    t = threading.Thread(target=process_frame, args=(f[1], f[0], p2_template, p2_coords))
+                    t = threading.Thread(target=process_frame, args=(f[1], f[0], [p1_template, p2_template, p3_template, p4_template], player_icon_coords))
                     threads.append(t)
                     t.start()
             
@@ -118,9 +142,9 @@ if __name__ == '__main__':
 
     sorted_start_times = sorted(game_start_times, key=lambda x: x[0])
 
-    for f,image in sorted_start_times:
+    for f,val,image in sorted_start_times:
         datetime_start_time = datetime.timedelta(seconds=f)
-        print(f"Game start at : {datetime_start_time}")
+        print(f"Game start at : {datetime_start_time}. Conf Value: {val}")
 
     elapsed_time = end_time - start_time
     print(f"Elapsed: {elapsed_time}")
