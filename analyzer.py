98fe20dbe81a5477c541609c44b62774343096d1
zephyruskk <zscott2212@gmail.com>
2023-06-01 15:07:48 -0400

    Determining best method for scraping tags and chars.

--- analyzer.py
+++ analyzer.py
@@ -1,25 +1,25 @@
 import sys, datetime, time, logging
 import cv2 as cv
 import numpy as np
-# import pytesseract
-import easyocr
+import pytesseract
 import pandas as pd
 import threading
 import multiprocessing as mp
 
+fps, frame_count = 0,0 # globals, for later
+temp_threshold = 0.75 # template threshold, be above this to clock player icons
 
-temp_threshold = 0.75
-# x1, y1 = 960,20
-# x2, y2 = 1080, 105
+# all coords are x1,y1,x2,y2
 
-p1_char_coords = 120,0,960,150
+p1_char_coords = 120,0,960,150 
 p2_char_coords = 1080,0,1919,150
 
 p1_tag_coords = 0,150,275,215
 p2_tag_coords = 960,150,1235,215
 
 go_coords = 495,195,1305,505 
-p1_coords = 20,40,80,90 # 10,20,100,100 # x1,y1,x2,y2
+
+p1_coords =  10,20,100,100 # 20,40,80,90 
 p2_coords = 980,40,1046,90 # 970,20,1060,100
 p3_coords = 19,40,85,90
 p4_coords = 980,40,1046,90
@@ -29,20 +29,27 @@ player_icon_coords = 0,0,1700,90
 batch_size = 16 # how many frames get checked at once (thread count!)
 batch_item_length = 30 # frames long
 
+# file paths
+vid_path = "./media/sample_0001.mkv"
+go_path = "./media/go.png"
+p1_path = "./media/p1.png"
+p2_path = "./media/p2.png"
+p3_path = "./media/p3.png"
+p4_path = "./media/p4.png"
 
-# state variables 
-game_start_times = []
-game_start_frames = False
+# state variables, [frame number, confidence value, np frame array]
+game_starts = []
 
+################################################################################################################################
+################################################################################################################################
 
-
+# detect if player icons are in a given frame
 def process_frame(frame, frame_number, templates, region_of_interest):
-    global game_start_times
+    global game_starts
 
     x1,y1,x2,y2 = region_of_interest
 
     roi = frame[y1:y2, x1:x2]
-
     roi = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
 
     for t in templates:
@@ -52,25 +59,20 @@ def process_frame(frame, frame_number, templates, region_of_interest):
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
         if max_val >= temp_threshold:
-            game_start_times.append((frame_number, max_val, frame))
+            game_starts.append((frame_number, max_val, frame))
             return True # match found 
         
     return False # no match found
 
-        
+################################################################################################################################
+################################################################################################################################
 
-# load in keyframes, batch_size at a time
 def process_video():
-
-    vid_path = "./media/sample_0003.mkv"
-    go_path = "./media/go.png"
-    p1_path = "./media/p1.png"
-    p2_path = "./media/p2.png"
-    p3_path = "./media/p3.png"
-    p4_path = "./media/p4.png"
+    global fps, frame_count, game_starts
 
     capture = cv.VideoCapture(vid_path)
-
+    
+    # reading in templates
     go_template = cv.imread(go_path)
     go_template = cv.cvtColor(go_template, cv.COLOR_BGR2GRAY)
     p1_template = cv.imread(p1_path)
@@ -82,8 +84,7 @@ def process_video():
     p4_template = cv.imread(p4_path)
     p4_template = cv.cvtColor(p4_template, cv.COLOR_BGR2GRAY)  # convert to grayscale
 
-    p2_t_h, p2_t_w = p2_template.shape[::1]
-
+    ## filling in video details ##
     if not capture.isOpened():
         print("Error opening video")
     else:
@@ -92,38 +93,34 @@ def process_video():
         frame_count = capture.get(cv.CAP_PROP_FRAME_COUNT)
         print(f"Frame count: {frame_count} frames")
 
-    global game_start_times
-    frame_no = 0
-    
+    frame_no = 0 # counting frames
     while capture.isOpened():
         try:
+            # grabbing frames
             frames = [None] * batch_size
 
             for i in range(len(frames)):
-                frame_grab = capture.grab()
+                frame_grab = capture.grab() 
                 ret, frame = capture.retrieve() # decode it!
                 frame_no += 1
                 if ret:
                     frames[i] = (frame_no, frame)
                 
+                # skip frames
                 for i in range(batch_item_length -1): # skip 29 frames
                     temp1 = capture.grab()
                     frame_no += 1
-            #         if not temp1:
-            #             if frame_no > frame_count:
-            #                 return
             
+            # processing frames
             threads = list()
             for f in frames:
                 if f is not None:
-                    # print(f"Thread for frame {f[0]} starting...")
-                    t = threading.Thread(target=process_frame, args=(f[1], f[0], [p1_template, p2_template, p3_template, p4_template], player_icon_coords))
+                    t = threading.Thread(target=process_frame, args=(f[1], f[0], [p1_template, p2_template, p3_template, p4_template], p1_coords))
                     threads.append(t)
                     t.start()
             
             for i,t in enumerate(threads):
-                t.join() # wait for the thread to terminate
-                # print(f"Thread {i} joined!")
+                t.join() 
             
             if frame_no >= frame_count - (batch_size * batch_item_length): # every batch, check if we're done
                 break # don't check for game starts in the last (batch_size * batch_item_length / fps) seconds MAX -- probably fine 
@@ -131,15 +128,14 @@ def process_video():
             print(e)
             break
     
-    # post processing
-    for i in range(len(game_start_times))[1:][::-1]: # cut off zero index, then reverse order
-        if game_start_times[i][0] - game_start_times[i-1][0] < fps*5: # if game start is within 5 seconds of the last game start, get rid of it
-            game_start_times.pop(i-1) # take the LATEST frame found to get the clearest possible frame 
-    
     capture.release()
     cv.destroyAllWindows()
 
-def scrape_keyframe(frame, reader):
+################################################################################################################################
+################################################################################################################################
+
+# getting tags and characters
+def scrape_keyframe(frame):
 
     # get character names + tags
     x1,y1,x2,y2 = p1_char_coords
@@ -160,37 +156,88 @@ def scrape_keyframe(frame, reader):
         noise = cv.medianBlur(gray, 3)
         threshold = cv.threshold(noise, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
         try:
-            result = reader.readtext(threshold, paragraph="False", detail=0)
-            print(result)
-            save_strings[i] = result[0]
+            result = pytesseract.image_to_string(threshold)
+            save_strings[i] = result.strip()
         except:
             print("No text found! No tag used?")
     
     return [(save_strings[0], save_strings[2]), (save_strings[1], save_strings[3])]
-    
+
+################################################################################################################################
+################################################################################################################################
+
+def calculate_image_clarity(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    laplacian = cv.Laplacian(gray, cv.CV_64F)
+    variance = np.var(laplacian)
+    min_c = np.min(gray)
+    max_c = np.max(gray)
+    contrast = (max_c - min_c)/(max_c + min_c)
+    return (variance, contrast)
+
+################################################################################################################################
+################################################################################################################################
 
 if __name__ == '__main__':
-
 
     start_time = time.time()
 
     process_video()
 
-    end_time = time.time()
+    sorted_start_times = sorted(game_starts, key=lambda x: x[0])
+    
+    # post processing: group frames into arrays
+    game_start_groupings = []
+    s = 0
+    for i in range(len(game_starts))[:-1]:
+        if game_starts[i+1][0] - game_starts[i][0] >= fps*5:
+            game_start_groupings.append(game_starts[s:(i+1)])
+            s = i+1
+    game_start_groupings.append(game_starts[s:])
 
-    sorted_start_times = sorted(game_start_times, key=lambda x: x[0])
+    output_filename = r'C:/Users/zscot/Videos/vodfixer/measures2.mp4'
+    codec = cv.VideoWriter_fourcc(*'mp4v')  # Choose the codec (e.g., 'XVID' for AVI, 'mp4v' for MP4)
+    fps = 30.0  # Frames per second
+    frame_width = 1920  # Width of the frames
+    frame_height = 1080  # Height of the frames    # post processing
+    out = cv.VideoWriter(output_filename, codec, fps, (frame_width, frame_height))
 
-    # pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_scale = 1.0
+    font_color = (0, 255, 0)  # BGR color tuple
+    line_thickness = 2
 
-    reader = easyocr.Reader(['en'])
+    for i in range(len(game_start_groupings)):
+        g = [frame[2] for frame in game_start_groupings[i]]
+        for f in g:
+            v,c = calculate_image_clarity(f)
+            cv.rectangle(f, (45,600),(350, 850), (0,0,0), cv.FILLED)
+            cv.putText(f, f"Group: {i}", (50,650), font, font_scale, font_color, line_thickness)
+            cv.putText(f, f"Variance: {v}", (50,700), font, font_scale, font_color, line_thickness)
+            cv.putText(f, f"Contrast: {c}", (50,750), font, font_scale, font_color, line_thickness)
+            out.write(f)
+    
+    out.release()
+    
+    exit()
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+    # reader = easyocr.Reader(['en'])
 
     for f,val,image in sorted_start_times:
         datetime_start_time = datetime.timedelta(seconds=f)
         print(f"Game start at : {datetime_start_time}. Conf Value: {val}")
+        # cv.imshow(f"{f}", image)
+        # key = cv.waitKey(0) & 0xFF
+        # if key == ord('s'):
+        #     cv.imwrite(f"C:/Users/zscot/Videos/vodfixer/{f}.png", image)
+        cv.destroyAllWindows()
 
-        print(scrape_keyframe(image, reader))
+        print(scrape_keyframe(image))
         # cv.imshow(f"{datetime_start_time}", image)
         # key = cv.waitKey(0) & 0xFF
 
+    end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed: {elapsed_time}")
